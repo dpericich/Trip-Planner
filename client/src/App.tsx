70ccda8c8ac6a5f22229b67e7835616373fa7b56
Daniel Pericich <dpericich@gmail.com>
2024-11-18 08:23:31 -0600

    Added styling and structure to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import NavBarContainer from './NavBar/NavBarContainer'
+import HomeScreen from './Screens/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="flex">
-        <div className="bg-red-800 w-24">
-          {/* 1/8 left bar */}
+    <div className="flex flex-row justify-start w-full h-screen">
+      <div className="bg-red-800 w-36">
+        <NavBarContainer />
+      </div>
+      <div className="bg-blue-800 w-full flex flex-col border-l-4 border-indigo-900">
+        {/* 7/8 main section */}
+        <div className="text-center bg-gray-300 text-3xl py-4">
+          {/* This will be based on the current url path */}
+          {/* 1/6 top tab */}
+          Home Tab
         </div>
-        <div className="bg-blue-800 w-full">
-          {/* 7/8 main section */}
-          <div className="">
-            {/* 1/6 top tab */}
-          </div>
-          <div className="">
-            {/* 5/6 main section */}
-          </div>
+        <div className="bg-green-700">
+          {/* 5/6 main section */}
+          <HomeScreen />
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
