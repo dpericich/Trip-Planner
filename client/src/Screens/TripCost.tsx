b6c28b10ae92d830f865b282ac7a0a397b48974c
Daniel Pericich <dpericich@gmail.com>
2024-11-18 08:22:17 -0600

    scaffolded TripCost element

---
+++ client/src/Screens/TripCost.tsx
@@ -0,0 +1,11 @@
+const TripCost = () => {
+    return (
+        <div className="">
+            I will include all the costs for the Trip
+            {/* Maybe each item has an optional cost input field and this will pull in that data to show running costs */}
+            {/* Could also use graphing to display categorical breakdown (bar and pie chart) */}
+        </div>
+    )
+};
+
+export default TripCost;
