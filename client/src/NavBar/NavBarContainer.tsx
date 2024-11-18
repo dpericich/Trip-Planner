73aa08edf861f28154b0c3534bca6d9b1bbed1de
Daniel Pericich <dpericich@gmail.com>
2024-11-18 08:23:09 -0600

    Added styling and placeholder elements to NavBarContainer component

--- client/src/NavBar/NavBarContainer.tsx
+++ client/src/NavBar/NavBarContainer.tsx
@@ -1,6 +1,23 @@
+import NavBarElement from "./NavBarElement";
+
+// I'm not sure there will be any props for this
+// Will use redux or some state store for getting session data? 
 const NavBarContainer = () => {
     return (
-        <div className=""></div>
+        // Will I have a static object I read in for this and just iterate over it?
+        <div className="flex flex-col h-full justify-start">
+            {/* This will need to have a prop for the icon file name */}
+            {/* Just store all the icons in the public directory */}
+            <NavBarElement title="Home" />
+            <NavBarElement title="Travel" />
+            <NavBarElement title="Accomodations" />
+            <NavBarElement title="Tours" />
+            <NavBarElement title="Dining" />
+            <NavBarElement title="Trip Cost" />
+            <div className="mt-auto bg-blue-100 py-4 px-2">
+                Trip Planner &copy;
+            </div>
+        </div>
     );
 };
 
