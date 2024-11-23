3b8b5e10c745bdd017938f4057b563d840495650
Daniel Pericich <dpericich@gmail.com>
2024-11-22 23:56:17 -0600

    added link as prop to NavBarElement and updated stubbed container values

--- client/src/NavBar/NavBarContainer.tsx
+++ client/src/NavBar/NavBarContainer.tsx
@@ -8,12 +8,13 @@ const NavBarContainer = () => {
         <div className="flex flex-col h-full justify-start">
             {/* This will need to have a prop for the icon file name */}
             {/* Just store all the icons in the public directory */}
-            <NavBarElement title="Home" iconPath="home" />
-            <NavBarElement title="Travel" iconPath="travel" />
-            <NavBarElement title="Lodgings" iconPath="hotel" />
-            <NavBarElement title="Tours" iconPath="tours" />
-            <NavBarElement title="Dining" iconPath="food" />
-            <NavBarElement title="Trip Cost" iconPath="money" />
+            <NavBarElement title="Home" link="/home" iconPath="home" />
+            <NavBarElement title="Itinerary" link="/itinerary" iconPath="itinerary" />
+            <NavBarElement title="Travel" link="/travel" iconPath="travel" />
+            <NavBarElement title="Lodgings" link="/lodgings" iconPath="hotel" />
+            <NavBarElement title="Tours" link="/tours" iconPath="tours" />
+            <NavBarElement title="Dining" link="/dining" iconPath="food" />
+            <NavBarElement title="Trip Cost" link="/trip-cost" iconPath="money" />
             <div className="mt-auto bg-blue-100 py-4 px-2">
                 Trip Planner &copy;
             </div>
--- client/src/NavBar/NavBarElement.tsx
+++ client/src/NavBar/NavBarElement.tsx
@@ -1,16 +1,17 @@
 interface NavBarElementProps {
     title: string,
+    link: string,
     iconPath: string
 }
 
-const NavBarElement = ({ title, iconPath }: NavBarElementProps) => {
+const NavBarElement = ({ title, link, iconPath }: NavBarElementProps) => {
     return (
         // Will need to make this a "Link" element
         // On hover change 
-        <div className="flex justify-between bg-blue-800 px-2 py-4 border-b-4 border-indigo-900">
+        <a href={link} className="flex justify-between bg-blue-800 px-2 py-4 border-b-4 border-indigo-900">
             {title}
             <img src={`./${iconPath}.png`} alt="" className="w-6" />
-        </div>
+        </a>
     );
 };
 
